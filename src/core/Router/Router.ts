e013433614336fd135d6c2135acff79de7e663c3
Timur359 <205208@mail.ru>
2024-06-05 15:13:09 +0300

    Выгрузил sprint 4

--- src/core/Router/Router.ts
+++ src/core/Router/Router.ts
@@ -1,5 +1,8 @@
 /* eslint-disable no-use-before-define */
-import { Block, Route } from "@Core";
+// eslint-disable-next-line no-restricted-imports
+import { Route } from "../Route";
+// eslint-disable-next-line no-restricted-imports
+import { Block } from "../Block";
 
 export class Router {
   static __instance: Router;
